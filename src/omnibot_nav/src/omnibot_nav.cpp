6d6c51438f3d50019b33213f0b496abd445e6881
Deval Shah <deval.maker@gmail.com>
2018-06-24 10:35:50 +0530

    Added package for navigation control.
    Added pseudo code for the controller.

---
+++ src/omnibot_nav/src/omnibot_nav.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <gazebo_msgs/GetModelState.h>
+
+int main (int argc, char** argv)
+{
+	ros::init(argc,argv,"omnibot_nav");
+	ros::NodeHandle n;
+
+//	Pseudo Code:
+//
+//	While ros shutdown
+//
+//		get_goal();
+//		get bot position
+//
+//		should bot move ?
+//
+//	    get intermediate goal (eliminate one (shortest dist))
+//			(if already at the intermediate pos, give the actual goal)
+//
+//		go_to_goal (x, y, theta) (send velocities towards goal)
+//			upper thresholds and lower thresholds to the output velos
+
+
+	return 0;
+
+}
