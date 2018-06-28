f8d3aceb0b3def56fac6ff8b6ee6c19a31c15e52
Deval Shah <deval.maker@gmail.com>
2018-06-28 11:53:21 +0530

    Added basic structure to goal_publisher node.

--- src/omnibot_nav/include/goal_pub.h
+++ src/omnibot_nav/include/goal_pub.h
@@ -1,3 +1,30 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
+#include <sensor_msgs/LaserScan.h>
 
+
+class goal_publisher
+{
+	private:
+
+		void laser_data_cb(const sensor_msgs::LaserScanConstPtr& scan);
+		void get_legs();
+		void compute_goal_pose();
+
+		ros::NodeHandle *node;
+		ros::Subscriber laser_sub;
+		ros::Publisher goal_pub;
+
+		sensor_msgs::LaserScan laser_data;
+		geometry_msgs::Pose goal_pose;
+
+		geometry_msgs::Point leg_points[4];
+		int8_t no_of_legs_detected;
+
+	public:
+
+		void get_goal();
+		void publish_goal();
+		goal_publisher(ros::NodeHandle* nodeH);
+		~goal_publisher();
+};
--- src/omnibot_nav/src/goal_pub.cpp
+++ src/omnibot_nav/src/goal_pub.cpp
@@ -1,34 +1,73 @@
 #include "goal_pub.h"
+#include "geometry_msgs/Point.h"
+
+const sensor_msgs::LaserScan scan_data;
 
 int main(int argc, char **argv)
 {
 
-	ros::init(argc, argv, "talker");
-
+	ros::init(argc, argv, "goal_publisher");
 	ros::NodeHandle n;
 
-	ros::Publisher chatter_pub = n.advertise<geometry_msgs::Pose>("/goal", 2);
+	goal_publisher gp(&n);
 
-	ros::Rate loop_rate(1);
-	int count = 0;
+	ros::Rate loop_rate(2);
 
 	while (ros::ok())
 	{
-		geometry_msgs::Pose msg;
-
-		msg.position.x = 1.0;
-		msg.position.y = 2.0;
-		msg.position.z = 3.0;
-		msg.orientation.w = 1;
-
-		ROS_INFO("Sending");;
-
-		chatter_pub.publish(msg);
-
 		ros::spinOnce();
+		ROS_INFO("Laser Message %f", scan_data.angle_max);
+
+		gp.get_goal();
+
+		gp.publish_goal();
 
 		loop_rate.sleep();
 	}
 
   return 0;
 }
+
+goal_publisher::goal_publisher(ros::NodeHandle *nodeH)
+{
+
+	this->node= nodeH;
+	this->no_of_legs_detected = 0;
+
+	this->laser_sub = node->subscribe("/laser/scan", 2, &goal_publisher::laser_data_cb, this);
+	this->goal_pub = node->advertise<geometry_msgs::Pose>("/goal", 2);
+
+}
+
+goal_publisher::~goal_publisher()
+{
+
+}
+
+void goal_publisher::get_goal()
+{
+	this->get_legs();
+	this->compute_goal_pose();
+}
+
+void goal_publisher::publish_goal()
+{
+	goal_pub.publish(this->goal_pose);
+}
+
+void goal_publisher::laser_data_cb(const sensor_msgs::LaserScanConstPtr& scan)
+{
+	this->laser_data = *scan;
+}
+
+void goal_publisher::get_legs()
+{
+
+}
+
+void goal_publisher::compute_goal_pose()
+{
+
+}
+
+
